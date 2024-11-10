8341feb675a7ede1b3d75100a0be697de7c165f2
LEE K.S <kingslee0619@gmail.com>
2024-11-11 02:53:59 +0900

    회원 등록 및 회원 정보 조회 관련 API 구현

---
+++ src/main/java/org/example/madeinha/domain/member/converter/MemberConverter.java
@@ -0,0 +1,34 @@
+package org.example.madeinha.domain.member.converter;
+
+import org.example.madeinha.domain.member.dto.response.MemberResponse;
+import org.example.madeinha.domain.member.entity.Member;
+import org.springframework.stereotype.Component;
+
+import static org.example.madeinha.domain.member.dto.response.MemberResponse.*;
+import static org.example.madeinha.domain.member.dto.response.MemberResponse.JoinInfo;
+
+@Component
+public class MemberConverter {
+
+    public JoinInfo toJoinInfo(Member member) {
+        return JoinInfo.builder()
+                .id(member.getMemberId())
+                .nickname(member.getNickname())
+                .createdAt(member.getCreateAt())
+                .build();
+    }
+
+    public MemberInfo toMemberInfo(Member member) {
+        return MemberInfo.builder()
+                .nickname(member.getNickname())
+                .point(member.getPoint())
+                .build();
+    }
+
+    public Member toEntity(String nickname) {
+        return Member.builder()
+                .nickname(nickname)
+                .point(0l)
+                .build();
+    }
+}
