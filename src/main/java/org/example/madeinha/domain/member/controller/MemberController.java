6beecfd562846528f1aa540c059f1af8d62be46f
LEE K.S <kingslee0619@gmail.com>
2024-11-11 03:03:26 +0900

    fix : 누락된 endpoint 추가

--- src/main/java/org/example/madeinha/domain/member/controller/MemberController.java
+++ src/main/java/org/example/madeinha/domain/member/controller/MemberController.java
@@ -20,11 +20,13 @@ public class MemberController {
     private final MemberService memberService;
     private final MemberConverter memberConverter;
 
+    @PostMapping("/join")
     public ResultResponse<JoinInfo> getJoinInfo(@RequestBody JoinRequest request) {
         Member member = memberService.join(request);
         return ResultResponse.of(SIGN_UP, memberConverter.toJoinInfo(member));
     }
 
+    @GetMapping("/info")
     public ResultResponse<MemberInfo> getMemberInfo(@RequestParam("memberId") Long memberId) {
         Member member = memberService.findMember(memberId);
         return ResultResponse.of(MEMBER_INFO, memberConverter.toMemberInfo(member));
