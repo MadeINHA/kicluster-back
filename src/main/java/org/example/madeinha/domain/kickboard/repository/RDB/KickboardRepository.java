c876529dfbc51ded54f30b24a54974e2b621d67a
LEE K.S <kingslee0619@gmail.com>
2024-11-25 16:25:09 +0900

    feat : kickboard엔티티 용 레포지토리 추가 (RDB, Redis 별도)

--- src/main/java/org/example/madeinha/domain/kickboard/repository/RDB/KickboardRepository.java
+++ src/main/java/org/example/madeinha/domain/kickboard/repository/RDB/KickboardRepository.java
@@ -4,6 +4,9 @@ import org.example.madeinha.domain.kickboard.entity.RDB.Kickboard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KickboardRepository extends JpaRepository<Kickboard, Long> {
+    List<Kickboard> findByClusterId(Integer clusterId);
 }
--- src/main/java/org/example/madeinha/domain/kickboard/repository/Redis/RedisKickboardRepository.java
+++ src/main/java/org/example/madeinha/domain/kickboard/repository/Redis/RedisKickboardRepository.java
@@ -3,8 +3,12 @@ package org.example.madeinha.domain.kickboard.repository.Redis;
 import org.example.madeinha.domain.kickboard.entity.Redis.RedisKickboard;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RedisKickboardRepository extends CrudRepository<RedisKickboard,Long> {
     Optional<RedisKickboard> findByKickboardId(Long kickboardId);
+
+    List<RedisKickboard> findByClusterId(Integer clusterId);
+
 }
