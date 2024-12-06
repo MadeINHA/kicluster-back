52a6b87a496e84f82d9d583eb6d05f50ecb61c4d
LEE K.S <kingslee0619@gmail.com>
2024-12-07 01:23:21 +0900

    feat : redis에 저장된 킥보드의 clusterId만 변경하는 기능 추가

---
+++ src/main/java/org/example/madeinha/global/redis/RedisService.java
@@ -0,0 +1,23 @@
+package org.example.madeinha.global.redis;
+
+import org.example.madeinha.domain.kickboard.entity.Redis.RedisKickboard;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+
+    private final HashOperations<String,String,Object> hashOperations;
+
+    @Autowired
+    public RedisService(RedisTemplate<String, Object> redisTemplate) {
+        this.hashOperations = redisTemplate.opsForHash();
+    }
+
+    public void updateClusterId(Long kickboardId, Integer newClusterId) {
+        String key = "kickboard:" + kickboardId;
+        hashOperations.put(key, "clusterId", newClusterId);
+    }
+}
