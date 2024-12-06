4dddc105761b0b7ba8e0b2dc3fec5dc168a56564
LEE K.S <kingslee0619@gmail.com>
2024-12-07 01:26:38 +0900

    fix : 기능이 겹쳐 불필요한 컨트롤러 삭제

--- src/main/java/org/example/madeinha/global/result/code/KickBoardResultCode.java
+++ src/main/java/org/example/madeinha/global/result/code/KickBoardResultCode.java
@@ -12,7 +12,9 @@ public enum KickBoardResultCode implements ResultCode {
     KICKBOARD_LOCATION_INFO(200, "SK001", "킥보드의 위치 조회에 성공하였습니다."),
     KICKBOARD_LIST_BY_CLUSTER_ID(200, "SK002", "클러스터id를 가진 모든 킥보드 조회에 성공하였습니다."),
     KICKBOARD_DETAIL_INFO(200, "SK003", "킥보드 상세 정보 조회를 성공하였습니다."),
-    ALL_KICKBOARD_LOCATION_INFO(200,"SK004","모든 킥보드의 위치 조회에 성공하였습니다.")
+    ALL_KICKBOARD_LOCATION_INFO(200, "SK004", "모든 킥보드의 위치 조회에 성공하였습니다."),
+    ALL_KCIKBOARD_INFO(200, "SK005", "모든 킥보드 상세 정보 조회에 성공하였습니다."),
+    LENT_KICKBOARD_TO_TOW_MODE(200, "SK006", "견인모드를 위한 킥보드 대여에 성공하였습니다."),
     ;
     private final int status;
     private final String code;
