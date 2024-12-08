8382e5d138c96dabb559dbc940dcfcf7cabfd815
LEE K.S <kingslee0619@gmail.com>
2024-12-09 05:02:11 +0900

    feat: 기존 구역 및 클러스터 엔티티 관련 결과 상태 코드 추가

--- src/main/java/org/example/madeinha/global/result/code/FixedAreaResultCode.java
+++ src/main/java/org/example/madeinha/global/result/code/FixedAreaResultCode.java
@@ -13,6 +13,7 @@ public enum FixedAreaResultCode implements ResultCode {
     EXIST_FIXED_AREA_INFO(200, "SA004", "모든 기존 주차 구역 정보 조회에 성공하였습니다."),
     ROAD_FIXED_AREA_INFO(200, "SA005", "모든 도로 구역 정보 조회에 성공하였습니다."),
     PROHIBIT_FIXED_AREA_INFO(200, "SA006", "모든 금지 구역 정보 조회에 성공하였습니다."),
+    NEAREST_EXIST_FIXED_AREA_INFO(200, "SA007", "가장 가까운 기존 주차 구역 조회에 성공하였습니다."),
     ;
     private final int status;
     private final String code;
