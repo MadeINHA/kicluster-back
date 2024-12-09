5c2bb4fcfc29e3ea1ec5ab02a6aad8facceb5fd6
LEE K.S <kingslee0619@gmail.com>
2024-12-10 02:35:26 +0900

    feat: 킥보드 에러코드 추가

--- src/main/java/org/example/madeinha/global/error/code/KickboardErrorCode.java
+++ src/main/java/org/example/madeinha/global/error/code/KickboardErrorCode.java
@@ -11,6 +11,7 @@ public enum KickboardErrorCode implements ErrorCode {
     KICKBOARD_NOT_FOUND_BY_ID(404, "EK000", "해당 id를 가진 킥보드가 존재하지 않습니다."),
     KICKBOARD_NOT_FOUND_BY_COORDINATE(404, "EK001", "해당 위치에 존재하는 킥보드가 존재하지 않습니다."),
     ALREADY_USING_KICKBOARD(404, "EK002", "이미 사용중인 킥보드 입니다."),
+    IS_NOT_TOW_KICKBOARD(404, "EK003", "견인 대상 킥보드가 아닙니다."),
     ;
     private final int status;
     private final String code;
