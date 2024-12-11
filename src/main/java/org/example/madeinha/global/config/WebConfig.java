11285fc592a79eefeb8a12c8fb05a4c7445392bf
LEE K.S <kingslee0619@gmail.com>
2024-12-12 01:59:03 +0900

    feat: 배포 페이지 변경에 따른 config 변경

--- src/main/java/org/example/madeinha/global/config/WebConfig.java
+++ src/main/java/org/example/madeinha/global/config/WebConfig.java
@@ -11,10 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
                 .allowedOrigins(
-                        "https://cartoondev.github.io",
-                        "https://dev-kicluster.vercel.app",
-                        "http://localhost:3000",
-                        "http://test.kij.surge.sh"
+                        "http://dev.kicluster.surge.sh",
+                        "http://localhost:3000"
                 ) // React 서버 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
